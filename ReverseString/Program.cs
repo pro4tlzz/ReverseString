4a686b730d569db77b2c149f25be949f6c92ae39
Pro4TLZZ <superfrillz12@hotmail.co.uk>
2018-08-15 11:11:29 +0100

    Initial commit

--- ReverseString/Program.cs
+++ ReverseString/Program.cs
@@ -10,6 +10,22 @@ namespace ReverseString
     {
         static void Main(string[] args)
         {
+            //Writes line explaining program
+            Console.WriteLine("This program will reverse what you wish to enter, type something below");
+            //Stores input to string
+            String userInput = Console.ReadLine();
+            //Convert string into array
+            char[] chaArray = userInput.ToCharArray();
+            //Reverse Array Contents
+            Array.Reverse(chaArray);
+            //Writes each character to line in a for loop
+            foreach (char userInputChar in chaArray)
+            {
+                Console.Write(userInputChar);
+            }
+            //Shows text and keeps console open
+            Console.ReadLine();
         }
+    
     }
 }
